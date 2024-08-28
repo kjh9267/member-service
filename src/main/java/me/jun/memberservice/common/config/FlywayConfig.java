abb87e1b8c09b7b7efe5026ec4a68b2d7dd2f445
kjh9267 <intellijunho@kakao.com>
2024-08-29 08:48:56 +0900

    feat: Create initial schema -feat: Create flyway config

---
+++ src/main/java/me/jun/memberservice/common/config/FlywayConfig.java
@@ -0,0 +1,25 @@
+package me.jun.memberservice.common.config;
+
+import org.flywaydb.core.Flyway;
+import org.springframework.boot.autoconfigure.flyway.FlywayProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableConfigurationProperties(FlywayProperties.class)
+public class FlywayConfig {
+
+    @Bean
+    public Flyway flyway(FlywayProperties flywayProperties) {
+        Flyway flyway = Flyway.configure()
+                .dataSource(flywayProperties.getUrl(), flywayProperties.getUser(), flywayProperties.getPassword())
+                .baselineOnMigrate(flywayProperties.isBaselineOnMigrate())
+                .baselineVersion(flywayProperties.getBaselineVersion())
+                .load();
+
+        flyway.repair();
+        flyway.migrate();
+        return flyway;
+    }
+}
